ecfdb517421f9095ca1fe17ea22d261725de5aff
alfatkhu.h <alfatkhu.h@primecash.co.id>
2022-04-20 15:01:01 +0700

    add framework

---
+++ widekyc.framework/Headers/widekyc.h
@@ -0,0 +1,18 @@
+//
+//  widekyc.h
+//  widekyc
+//
+//  Created by H, Alfatkhu on 24/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for widekyc.
+FOUNDATION_EXPORT double widekycVersionNumber;
+
+//! Project version string for widekyc.
+FOUNDATION_EXPORT const unsigned char widekycVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <widekyc/PublicHeader.h>
+
+
